4b9f3446957fded332442e69594eb8d830fee019
re-shimomura <soranosanpo07@gmail.com>
2024-06-12 17:07:52 +0900

    BANされたユーザのログインをはじく

--- login.php
+++ login.php
@@ -8,49 +8,53 @@
     $user = "testuser";
     $pass = "testpass";
 
-try {
-    // DBに接続します
-    $dbh = new PDO($dsn, $user, $pass);
+    try {
+        // DBに接続します
+        $dbh = new PDO($dsn, $user, $pass);
 
-    // フォームから送信されたデータを受け取る処理
-    if (isset($_POST["userid"])) {
-        $userid = $_POST["userid"];
-        $password = $_POST["password"];
+        // フォームから送信されたデータを受け取る処理
+        if (isset($_POST["userid"])) 
+        {
+            $userid = $_POST["userid"];
+            $password = $_POST["password"];
 
-        // SQL文の用意
-        $sql = <<<sql
-        
-        SELECT * FROM users WHERE userid = ? AND password = ? 
-        
+            // SQL文の用意
+            $sql = <<<sql
+            SELECT * FROM users WHERE userid = ? AND password = ? 
+            
 sql;
 
-        // プレイスホルダー
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(1, $userid);
-        $stmt->bindParam(2, $password);
-        $stmt->execute();
+            // プレイスホルダー
+            $stmt = $dbh->prepare($sql);
+            $stmt->bindParam(1, $userid);
+            $stmt->bindParam(2, $password);
+            $stmt->execute();
 
 
-        // 結果を取得
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        if ($result[0]["permission"] == "admin" ){
-          header('Location: admin.php');
-          exit;
-        }else if ($stmt->rowCount() > 0) {
-            // echo "ログイン成功";
-            // ログイン成功時の処理をここに記術
-            $_SESSION["userid"] = $userid;
+            // 結果を取得
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+            //flagが1だったらログインできないようにする
+            if (isset($result[0]["flag"]) && $result[0]["flag"] == 1){
+                echo "無効なアカウントです";
+                //header('Location: login.html');
+                exit;
+            }
 
-            header('Location: selectMenu.html');
-
-        }else{
-            echo "ユーザー名またはパスワードが間違っています";
+            if ($result[0]["permission"] == "admin" ){
+                header('Location: admin.php');
+                exit;
+            }else if ($stmt->rowCount() > 0) {
+                // echo "ログイン成功";
+                // ログイン成功時の処理をここに記術
+                $_SESSION["userid"] = $userid;
+                header('Location: selectMenu.html');
+            }else{
+                echo "ユーザー名またはパスワードが間違っています";
+            }
         }
+    } catch (PDOException $e) {
+        echo "接続失敗・・・";
+        echo "エラー内容：" . $e->getMessage();
     }
-} catch (PDOException $e) {
-    echo "接続失敗・・・";
-    echo "エラー内容：" . $e->getMessage();
-}
 ?>
